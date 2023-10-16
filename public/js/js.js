721ad4ddaf06715f0fd585852fe4e42406cde055
crisaspiutn <caspivillegas@frba.utn.edu.ar>
2023-10-16 19:33:00 -0300

    min fix in carousel

--- public/js/js.js
+++ public/js/js.js
@@ -1,7 +1,8 @@
-let btn_menu=document.querySelector(".btn_menu");
-let menu=document.querySelector(".menu");
+const btn_menu=document.querySelector(".btn_menu");
+const menu=document.querySelector(".menu");
 btn_menu.addEventListener("click",()=>{
     menu.classList.toggle("ocultar_menu");
     btn_menu.classList.toggle("icon-menu");
     btn_menu.classList.toggle("icon-close");
 });
+
