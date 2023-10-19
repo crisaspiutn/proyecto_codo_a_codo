db9ce660e4af3fc27843cec91cce92272328f266
crisaspiutn <caspivillegas@frba.utn.edu.ar>
2023-10-19 02:59:19 -0300

    arregle todos los carruseles

--- public/js/js.js
+++ public/js/js.js
@@ -1,4 +1,4 @@
-const nuevo_efecto=document.querySelector("#nuevo_efecto");
+const proyectos=document.querySelector(".proyectos");
 const btn_menu=document.querySelector(".btn_menu");
 const menu=document.querySelector(".menu");
 btn_menu.addEventListener("click",()=>{
@@ -7,3 +7,41 @@ btn_menu.addEventListener("click",()=>{
     btn_menu.classList.toggle("icon-close");
 });
 
+// proyectos.addEventListener("click",(e)=>{
+//     console.log(e.target.dataset["slideTo"]);
+//     console.log(e.target.dataset);
+//     // console.log(e.target.attributes);
+//     if(e.target.dataset.hasOwnProperty("slideTo")){
+//         console.log(e.target.parentNode.children);
+//         console.log("encontrado");
+//         let lista=Object.values(e.target.parentNode.children);
+//         lista.forEach(item=>{
+//             console.log(item);
+//             if(item.classList.contains("active")){
+//                 item.classList.remove("active");
+//             }
+//         })
+//         e.target.classList.add("active");
+//     }
+// });
+
+
+
+
+
+
+
+function efectoCarrusel(){
+    if(e.target.dataset.hasOwnProperty("slideTo")){
+        console.log(e.target.parentNode.children);
+        console.log("encontrado");
+        let lista=Object.values(e.target.parentNode.children);
+        lista.forEach(item=>{
+            console.log(item);
+            if(item.classList.contains("active")){
+                item.classList.remove("active");
+            }
+        })
+        e.target.classList.add("active");
+    }
+}
